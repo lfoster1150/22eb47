6f062aaba6e60d3e17b2fac55cfdaee2922e0a96
lfoster1150 <lfoster1150@gmail.com>
2021-12-15 15:31:12 -0600

    Message added to component immidiately on user input for new conversations

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -72,7 +72,7 @@ export const addNewConvoToStore = (state, recipientId, message) => {
       convo.id = message.conversationId;
       convo.messages.push(message);
       convo.latestMessageText = message.text;
-      return convo;
+      return {...convo};
     } else {
       return convo;
     }
