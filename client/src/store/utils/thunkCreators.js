062ed3eefd8a42cbc4ec23a2f18270d8aa5f837c
lfoster1150 <lfoster1150@gmail.com>
2021-12-17 09:59:12 -0600

    Revert line 102 of thunkCreators.js

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -99,7 +99,7 @@ export const postMessage = (body) => async (dispatch) => {
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
-      dispatch(setNewMessage(data.message, data.sender));
+      dispatch(setNewMessage(data.message));
     }
 
     sendMessage(data, body);
